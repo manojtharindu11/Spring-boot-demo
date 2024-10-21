aa000a15af322d7e8a2c1e5f35bcc12aead3678b
Tharindu Manoj Thilakarathna <thilakarathnawatm.21@uom.lk>
2024-10-21 19:23:43 +0530

    create constructors using lombok annotations

--- src/main/java/com/example/demo/dto/UserDto.java
+++ src/main/java/com/example/demo/dto/UserDto.java
@@ -1,5 +1,12 @@
 package com.example.demo.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserDto {
     private int id;
     private String name;
