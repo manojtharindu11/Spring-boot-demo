9fc1f25bdc84e86c5bec6ce755705b2efcc303be
Tharindu Manoj Thilakarathna <thilakarathnawatm.21@uom.lk>
2024-10-21 23:23:37 +0530

    create add user controller

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,17 +1,26 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.dto.UserDto;
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin()
 @RequestMapping(value = "api/v1")
 public class UserController {
+    @Autowired
+    private UserService userService;
 
-    @GetMapping("/getUser")
-    public String getUser() {
-        return "Hello World";
+    @GetMapping("/getusers")
+    public List<UserDto> getUser() {
+        return userService.getAllUsers();
+    }
+
+    @PostMapping("/adduser")
+    public UserDto addUser(@RequestBody UserDto userDto) {
+        return userService.addUser(userDto);
     }
 }
